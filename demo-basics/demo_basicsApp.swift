8402875e0bd5f63ff095f5dce7edc36933f3a767
Paulo Orquillo <paulo.orquillo@gmail.com>
2020-11-26 23:22:24 +1300

    Initial Commit

---
+++ demo-basics/demo_basicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  demo_basicsApp.swift
+//  demo-basics
+//
+//  Created by Paulo Orquillo on 26/11/20.
+//
+
+import SwiftUI
+
+@main
+struct demo_basicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
